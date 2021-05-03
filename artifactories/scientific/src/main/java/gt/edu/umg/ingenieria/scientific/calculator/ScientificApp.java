c37514701807da57ff30e0fdad88d24c10b7f855
Erick Mejia <mejigalveze@outlook.com>
2021-05-02 23:48:38 -0600

    Scientific calculator with all the operations requested

---
+++ artifactories/scientific/src/main/java/gt/edu/umg/ingenieria/scientific/calculator/ScientificApp.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.scientific.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScientificApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScientificApp.class, args);
+	}
+
+}
