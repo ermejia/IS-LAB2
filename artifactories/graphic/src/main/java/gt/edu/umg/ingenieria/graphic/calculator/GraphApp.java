fa6b50556f6fb3ea8507597407134cd7547d9e60
Erick Mejia <mejigalveze@outlook.com>
2021-05-03 00:05:29 -0600

    Graphic Calculator creation

---
+++ artifactories/graphic/src/main/java/gt/edu/umg/ingenieria/graphic/calculator/GraphApp.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.graphic.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphApp.class, args);
+	}
+
+}
